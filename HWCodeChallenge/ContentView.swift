b53f5dd1a44a2ef607319af1b58077551f538f62
Camilo Masso <14898956+camilomq@users.noreply.github.com>
2025-10-28 12:03:47 -0500

    Initial Commit

---
+++ HWCodeChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HWCodeChallenge
+//
+//  Created by Camilo Masso on 28/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HWCodeChallenge/HWCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWCodeChallengeApp.swift
+//  HWCodeChallenge
+//
+//  Created by Camilo Masso on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HWCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
